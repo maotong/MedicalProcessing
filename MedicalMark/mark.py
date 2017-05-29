62d4b948509d5e642a78d8c835b7ca7c57ef513b
Jianyu.Wang <873869027@qq.com>
2017-05-29 18:34:01 +0800

    Clean Edge Hough Circle Update.

--- MedicalMark/mark.py
+++ MedicalMark/mark.py
@@ -7,6 +7,7 @@ import numpy as np
 # 图像预处理，获得图像中红色的元素
 img = cv2.imread("test3.jpg")
 
+# pre-processing image
 row = img.shape[0]
 # print row
 col = img.shape[1]
@@ -30,13 +31,35 @@ for i in range(1, row):
         emptyImage[i, j] = diff1 if diff1 < diff2 else diff2
 
 cv2.imwrite("test.jpg", emptyImage)
+# pre-processing image finished
+
+# Clean Edge
+left = 0.075
+right = 0.075
+top = 0.15
+bottom = 0.15
+
+edge = [left * row,
+        row - right * row,
+        top * col,
+        col - bottom * row]
+
+for i in range(1, row):
+    for j in range(1, int(edge[0])):
+        emptyImage[i, j] = 0
+    for j in range(int(edge[1]), col):
+        emptyImage[i, j] = 0
+
+for j in range(1, col):
+    for i in range(1, int(edge[2])):
+        emptyImage[i, j] = 0
+    for i in range(int(edge[3]), row):
+        emptyImage[i, j] = 0
+
+cv2.imwrite("test.jpg", emptyImage)
 
 
 
-
-
-# opencv image show in python
-# BGR image
 
 
 
